771aafd80b7196e2fb4b55ef8327a0438305c2f0
William Sundqvist <William.Sundqvist@sogeti.com>
2023-12-05 23:24:13 +0100

    Done with 5.1 with a recursive approach

--- 5/1.ts
+++ 5/1.ts
@@ -20,104 +20,53 @@ var orderIndex = 0;
 var sum = 0;
 var seeds: string[] = [];
 
-var seedToSoilMap: MapRow[] = [];
-var soilToFertMap: MapRow[] = [];
-var fertToWaterMap: MapRow[] = [];
-var waterTolightMap: MapRow[] = [];
-var lightToTempMap: MapRow[] = [];
-var TempToHumMap: MapRow[] = [];
-var HumToLocMap: MapRow[] = [];
+var seedString = lines[0].split(": ")[1];
+seeds = seedString.split(" ");
 
-var seedsAreDone = false;
+var result = finalDestinationFromSource(seeds, [], 3)
+console.log(result);
+console.log(result.sort((a,b) => +a - +b)[0])
 
-for (var [i, l] of lines.entries())
+
+function finalDestinationFromSource(input: string[], nextInput: string[], index: number)
 {
+    if (index == lines.length)
+    {
+        for (var object of input)
+        {
+            nextInput.push(object);
+        }
+        return nextInput;
+    }
+    if (lines[index] == "")
+    {
+        for (var object of input)
+        {
+            nextInput.push(object);
+        }
+        return finalDestinationFromSource(nextInput, [], index + 1)
+    }
+    if (lines.includes(":"))
+        return finalDestinationFromSource(input, nextInput, index + 1);
+    
+    var map = lines[index];
+    var splitMap = map.split(" ");
+    var destinationStart = +splitMap[0];
+    var sourceStart = +splitMap[1];
+    var range = +splitMap[2];
+    for (var s of input)
+    {
+        //Seed has to be larger than start
+        var seed = +s;
+        if (+seed >= sourceStart && +seed < sourceStart + range)
+        {
+            var distanceFromStart = seed - sourceStart;
+            var destinationValue = destinationStart + distanceFromStart;
+            input = input.filter((o) => o != s);
+            nextInput.push("" + destinationValue);
+        }
+    }
+    return finalDestinationFromSource(input, nextInput, index + 1);
 
-    if (l == "")
-        continue;
-
-    const status = ORDER[orderIndex];
-
-    if (status == SEEDS && !seedsAreDone)
-    {
-        var seedString = l.split(": ")[1];
-        console.log(seedString);
-        seeds = seedString.split(" ");
-        seedsAreDone = true;
-        continue;
-    }
-
-    if (l.includes(":"))
-        orderIndex++;
-
-    if (status == SEED_TO_SOIL)
-    {
-        addToMap(seedToSoilMap, l);
-    }
-    if (status == SOIL_TO_FERT)
-    {
-        addToMap(soilToFertMap, l);
-    }
-    if (status == FERT_TO_WATER)
-    {
-        addToMap(fertToWaterMap, l);
-    }
-    if (status == WATER_TO_LIGHT)
-    {
-        addToMap(waterTolightMap, l);
-    }
-    if (status == LIGHT_TO_TEMP)
-    {
-        addToMap(lightToTempMap, l);
-    }
-    if (status == TEMP_TO_HUM)
-    {
-        addToMap(TempToHumMap, l);
-    }
-    if (status == HUM_TO_LOC)
-    {
-        addToMap(HumToLocMap, l);
-    }
 }
-
-//Time to get the locations for the seeds
-var locations: string[] = [];
-var minLoc = Number.MAX_VALUE;
-for (var seed of seeds)
-{
-    var soil = getDestination(seed, seedToSoilMap);
-    var fert = getDestination(soil, soilToFertMap);
-    var water = getDestination(fert, fertToWaterMap);
-    var light = getDestination(water, waterTolightMap);
-    var temp = getDestination(light, lightToTempMap);
-    var hum = getDestination(temp, TempToHumMap);
-    var location = getDestination(hum, HumToLocMap);
-    if (+location < minLoc)
-        minLoc = +location;
-    locations.push(location);
-}
-console.log(locations);
-console.log(minLoc)
-
-
-
-
-function addToMap(map: MapRow[], line: string)
-{
-       var parts = l.split(" ");
-       var destinationStart = +parts[0];
-       var sourceStart = +parts[1];
-       var range = +parts[2];
-       for(var i=0; i<range; i++)
-       {
-            var mapRow: MapRow = {source: ""+(sourceStart + i), destination: ""+(destinationStart + i) }
-            map.push(mapRow);
-       }
-}
-
-function getDestination(value: string, map: MapRow[]): string
-{
-    return map.find(e => e.source == value)?.destination ?? value;
-}
-
 
